7bfed070ce4c7450d55318a013faa74835f97c5d
Tam Nguyen <npt.dc@outlook.com>
2025-04-02 13:58:38 +0700

    Switch to ChatDeepSeek for LLM interactions
    Replaced BaseChatOpenAI with ChatDeepSeek to leverage the dedicated DeepSeek integration. Adjusted the example code for message-based interaction, showcasing translation functionality. This change enhances compatibility with DeepSeek's API and simplifies usage.

--- llm/notebook/agents/llm_providers/deepseek_langchain.py
+++ llm/notebook/agents/llm_providers/deepseek_langchain.py
@@ -1,13 +1,35 @@
 from dotenv import load_dotenv
 from langchain_openai.chat_models.base import BaseChatOpenAI
+from langchain_deepseek import ChatDeepSeek
 
 load_dotenv()
 
-llm = BaseChatOpenAI(
-    model='deepseek-chat',
-    base_url='https://api.deepseek.com',
-    max_tokens=1024
+# llm = BaseChatOpenAI(
+#     model='deepseek-chat',
+#     base_url='https://api.deepseek.com',
+#     max_tokens=1024
+# )
+#
+# response = llm.invoke("Hi!")
+# print(response.content)
+
+
+
+llm = ChatDeepSeek(
+    model="deepseek-chat",
+    temperature=0,
+    # max_tokens=None,
+    # timeout=None,
+    # max_retries=2,
+    # other params...
 )
 
-response = llm.invoke("Hi!")
-print(response.content)
+messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    ("human", "I love programming."),
+]
+ai_msg = llm.invoke(messages)
+print(ai_msg.content)
