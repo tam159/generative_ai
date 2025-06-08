f6c7c128d1c958ebc70e41e6f91dbdf5cf437b34
Tam Nguyen <npt.dc@outlook.com>
2025-06-08 10:20:37 +0700

    Update GoogleGenerativeAI model to gemini-2.5-pro-preview-06-05

--- llm/notebook/agents/llm_providers/google_ai_studio.py
+++ llm/notebook/agents/llm_providers/google_ai_studio.py
@@ -4,7 +4,7 @@ from langchain_google_genai import GoogleGenerativeAI
 
 load_dotenv()
 
-llm = GoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.0)
+llm = GoogleGenerativeAI(model="gemini-2.5-pro-preview-06-05", temperature=0.0)
 print(
     llm.invoke(
         "What are some of the pros and cons of Python as a programming language?"
